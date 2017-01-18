111968643afd706c370a1c9016ee41830afc60d3
Patrick Woods <patrickowoods@gmail.com>
2017-01-18 21:05:04 +1100

    final(?) critter farm program. Added original critter to the farm.

--- critter_farm.cpp
+++ critter_farm.cpp
@@ -1,6 +1,7 @@
-//Critter Farm
+// Critter Farm
 // Demonstrates object containment
-// Written by Patrick Woods 18/1/2017 from Beginning C++ Through Game Programming Third Edition, Michael Dawson
+// Written by Patrick Woods 18/1/2017
+// from Beginning C++ Through Game Programming Third Edition, Michael Dawson
 
 #include <iostream>
 #include <string>
@@ -12,16 +13,21 @@ using std::vector;
 class Critter
 {
 public:
-	Critter(const string& name = "");
+	static int s_Total;	// static data member variable declaration, counts total number of critters
+	Critter(const string& name = "");	// Constructor prototype
 	string GetName() const;
 
 private:
-	string m_Name;
+	string m_Name;	// An example of aggregation
 };
 
-Critter::Critter(const string& name):
+int Critter::s_Total = 0;	// Always initialize static variables before incrementing, stupid!
+
+Critter::Critter(const string& name):	/* Constructor definition. By using the colon, the author uses member initializers as a shorthand way to assign data values. If there were multiple data members, they would be separated by commas. */
 	m_Name(name)
-{}
+{		// The constuctor body
+	++ s_Total;
+}
 
 inline string Critter::GetName() const
 {
@@ -31,12 +37,12 @@ inline string Critter::GetName() const
 class Farm
 {
 public:
-	Farm(int spaces = 1);
-	void Add(const Critter& aCritter);
+	Farm(int spaces = 1);	// Constructor prototype, default size of 1
+	void Add(const Critter& aCritter);	// Arg is read only of type = Critter
 	void RollCall() const;
 
 private:
-	vector<Critter> m_Critters;
+	vector<Critter> m_Critters;		// m_Critters is a vector containing the farm's Critter population
 };
 
 Farm::Farm(int spaces)
@@ -46,16 +52,17 @@ Farm::Farm(int spaces)
 
 void Farm::Add(const Critter& aCritter)
 {
-	m_Critters.push_back(aCritter);
+	m_Critters.push_back(aCritter);	// m_Critters is a vector, so we use the push_back() function to add new critters to the farm
 }
 
 void Farm::RollCall() const
 {
+	// Because m_Critters is of type = vector<Critter>, that is to say a vector of Critters, our iterator (iter) must also be a constant iterator (const_iterator) within the namespace vector<Critter>. We can use begin() and end() to find the start and end of our vector.
 	for (vector<Critter>::const_iterator iter = m_Critters.begin();
 			iter != m_Critters.end();
 			++iter)
 	{
-		std::cout << iter->GetName() << " here.\n";
+		std::cout << iter->GetName() << " here.\n";	// This line suggests that iter is a pointer to a critter in the vector.
 	}
 }
 
@@ -65,9 +72,10 @@ int main()
 	std::cout << "My critter's name is " << crit.GetName() << std::endl;
 
 	std::cout << "\nCreating critter farm.\n";
-	Farm myFarm(3);
+	Farm myFarm(4);
 
-	std::cout << "\nAdding three critters to the farm.\n";
+	std::cout << "\nAdding three new critters to the farm.\n";
+	myFarm.Add(crit);
 	myFarm.Add(Critter("Moe"));
 	myFarm.Add(Critter("Larry"));
 	myFarm.Add(Critter("Curly"));
