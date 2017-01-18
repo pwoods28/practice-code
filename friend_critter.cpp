087b14d21104a3b4b319aaefa1f712f2fdf3497b
Patrick Woods <patrickowoods@gmail.com>
2017-01-18 22:35:55 +1100

    Added friend_critter.cpp

---
+++ friend_critter.cpp
@@ -0,0 +1,48 @@
+// Friend Critter
+// Demonstrates friend functions and operator level overloading//
+// Written by Patrick Woods 18/1/2017 using work from Beginning C++ Through Game Programming, Third Edition, Michael Dawson.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Critter
+{
+	friend void Peek(const Critter& aCritter);	// Peek gets aCritter by const reference, so a copy of the object is not made (benefit of passing by reference!) but the object is also protected against unwanted changes (by being const). Win win win win!
+	friend ostream& operator<<(ostream& os, const Critter& aCritter);
+
+public:
+	Critter(const string& name = "");
+
+private:
+	string m_Name;
+};
+
+Critter::Critter(const string& name):
+	m_Name(name)
+{}
+
+void Peek(const Critter& aCritter);
+ostream& operator<<(ostream& os, const Critter& aCritter);
+
+int main()
+{
+	Critter crit("Poochie");
+
+	cout << "Calling Peek() to access crit's private data member, m_Name: \n";
+	Peek(crit);
+
+	cout << "\nSending crit object to cout with the << operator:\n";
+	cout << crit;
+
+	return 0;
+}
+
+//global friend function which can access all of Critter object's members. Overloads the << operator so you can send a Critter object to cout
+ostream& operator<<(ostream& os, const Critter& aCritter)
+{
+	os << "Critter Object - ";
+	os << "m_Name: " << aCritter.m_Name;
+	return os;
+}
